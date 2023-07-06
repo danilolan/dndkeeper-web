a0d34509d049b36314ae20a3c25ce57ada1b8283
Matheus Paulo <matheusslg1@gmail.com>
2023-07-06 15:30:04 -0300

    feat: Player and DM Card Component

--- app/components/atoms/Button.tsx
+++ app/components/atoms/Button.tsx
@@ -1,31 +1,31 @@
-import React from 'react';
-import clsx from 'clsx';
+import React from "react";
+import clsx from "clsx";
 
-type Props = React.ButtonHTMLAttributes<HTMLButtonElement>
+type Props = React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-export const Button = ({ className, ...rest}: Props) => {
+export const Button = ({ className, ...rest }: Props) => {
   return (
-    <button 
-        className={clsx(
-            'text-white-100 bg-gradient-to-r from-primary',
-            'h-10 px-32',
-            'border border-white-400', 
-            'rounded-3xl',
-            'cursor-pointer',
-            'relative shadow', 
-            // Hover
-            'transition',
-            'hover:scale-105 hover:shadow-md',
-            // Focus
-            'outline-none',
-            'ring-primary/70 ring-offset-2',
-            'focus-visible:ring-2 focus:scale-[0.98]',
-            // Disabled
-            'disabled:bg-gradient-primary-disabled disabled:cursor-not-allowed ',
-            className
-        )}
-        disabled={rest.disabled} 
-        {...rest} 
+    <button
+      className={clsx(
+        "text-white-100 bg-gradient-to-r from-primary",
+        "h-10 px-32",
+        "border border-white-400",
+        "rounded-3xl",
+        "cursor-pointer",
+        "relative shadow",
+        // Hover
+        "transition",
+        "hover:scale-105 hover:shadow-md",
+        // Focus
+        "outline-none",
+        "ring-primary/70 ring-offset-2",
+        "focus-visible:ring-2 focus:scale-[0.98]",
+        // Disabled
+        "disabled:bg-gradient-primary-disabled disabled:cursor-not-allowed ",
+        className
+      )}
+      disabled={rest.disabled}
+      {...rest}
     />
-  )
+  );
 };
--- app/components/molecules/PlayerCard.tsx
+++ app/components/molecules/PlayerCard.tsx
@@ -4,81 +4,81 @@ import Image, { StaticImageData } from "next/image";
 import character from "../../../public/images/wizard.png";
 import master from "../../../public/images/master.png";
 
-interface CardProps {
+interface PlayerCardProps {
   character_name?: string;
   character_class?: string;
-  character_image?: StaticImageData;
-  race?: string;
-  level?: string;
+  character_race?: string;
+  character_image: StaticImageData;
+  character_level?: string;
   date?: string;
   table_name?: string;
   master_name?: string;
-  master_image?: StaticImageData;
+  master_image: StaticImageData;
 }
 
-export const Card: React.FC<CardProps> = ({
+export const PlayerCard: React.FC<PlayerCardProps> = ({
   character_name,
   character_class,
+  character_race,
   character_image,
-  race,
-  level,
+  character_level,
   date,
   table_name,
   master_name,
   master_image,
 }) => {
   return (
-    <div className="max-w-sm py-4 min-w-[300px] bg-white-600 border-2 border-white-300 rounded-3xl drop-shadow-primary">
-
+    <>
       <div className="flex pl-1">
-
-        <div className="rounded-full overflow-hidden">
+        <div className="w-28 rounded-full overflow-hidden">
           <Image
-            src={character}
+            src={character_image}
             alt={"player character"}
             className="object-cover w-full h-full"
           />
         </div>
 
         <div className="flex flex-col justify-center ">
-          <h2 className="text-base font-bold text-black-900 mb-2 line-clamp-1 max-w-[160px]">
-            Baragor Ironfirst
+          <h2 className="text-base font-bold text-black-900 mb-2 line-clamp-1 max-w-[175px]">
+            {character_name}
           </h2>
           <div className="flex flex-row gap-6">
             <p className="text-xs font-medium text-black-400 mb-1">
-              Elf, Wizard
+              {character_race}, {character_class}
             </p>
-            <p className="text-xs font-medium text-black-400">( Level 4 )</p>
+            <p className="text-xs font-medium text-black-400">
+              ( Level {character_level} )
+            </p>
           </div>
         </div>
-
       </div>
 
       <hr className="mt-4 border-white-300" />
 
       <div className="flex flex-row pl-1 pr-8 mt-2 justify-between">
         <div className="flex flex-col text-center items-center">
-          <h2 className="text-lg font-medium leading-3 mt-2">Mesa 1</h2>
-          <p className="text-xxs text-black-400 font-medium">Last played at 23/04/2023</p>
+          <h2 className="text-lg font-medium leading-3 mt-2">{table_name}</h2>
+          <p className="text-xxs text-black-400 font-medium">
+            Last played at {date}
+          </p>
         </div>
 
         <div className="flex flex-col gap-1">
-          <h3 className="text-xs text-bordo-400 font-semibold leading-4">Dungeon Master</h3>
+          <h3 className="text-xs text-bordo-400 font-semibold leading-4">
+            Dungeon Master
+          </h3>
           <div className="flex flex-row items-center gap-1">
-            <div className=" w-6 h-6 rounded-full overflow-hidden">
+            <div className="w-6 h-6 rounded-full overflow-hidden">
               <Image
-                src={master}
+                src={master_image}
                 alt={"dungeon master picture"}
                 className="object-cover w-full h-full"
               />
             </div>
-            <p className="text-xs font-medium">Porto</p>
+            <p className="text-xs font-medium">{master_name}</p>
           </div>
         </div>
       </div>
-
-        
-
-    </div>
+    </>
   );
 };
